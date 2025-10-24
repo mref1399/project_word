e754b98a7e568f2ab85f3316a3339f08943da02d
mref1399 <79265175+mref1399@users.noreply.github.com>
2025-10-24 22:09:00 +0330

    Update create_word.py

--- create_word.py
+++ create_word.py
@@ -325,4 +325,4 @@ def home():
     return jsonify({'message': 'Persian DOCX Generator — Safe Mode ✅'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8001)
